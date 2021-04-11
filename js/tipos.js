425b716223bd79c709e8e128453925ce7925b8c6
Elizabethproyectofinal <82339144+Elizabethproyectofinal@users.noreply.github.com>
2021-04-11 15:10:55 -0500

    Create tipos.js

---
+++ js/tipos.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef {Object} Rol
+ * @property {string} descripción
+ */
+
+/**
+ * @typedef {Object} Alumno
+ * @property {string} nombre
+ */
+
+/**
+ * @typedef {Object} Usuario
+ * @property {string[]} rolIds
+ * @property {string} alumnoId
+ */
+
+// @ts-nocheck
+/**
+ * @typedef {Object} Mensaje
+ * @property {string} usuarioId
+ * @property {string} texto
+ * @property {firebase.firestore.Timestamp} timestamp
+ */
+
+export const __tipos = 0;
+© 2021 GitHub, Inc.
