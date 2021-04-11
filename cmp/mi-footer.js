ac9125e6af315d5454c730871cceca609ac6e42e
Elizabethproyectofinal <82339144+Elizabethproyectofinal@users.noreply.github.com>
2021-04-11 16:04:02 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,1 +1,13 @@
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<p>
+        &copy; 2021
+        Brito Bonilla Elizabeth
+      </p>`;
+  }
+}
 
+customElements.define(
+  "mi-footer", MiFooter);
